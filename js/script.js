5b5e98a77b22d8f4d79c5e31d7e6a3f56fab3c10
omsoffset <163588270+omsoffset@users.noreply.github.com>
2024-03-20 01:15:47 +0530

    form update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
-const contactForm = document.getElementById('contact-form')
+const contactForm = document.getElementById('contact-form'),
+      nameEl = document.getElementById('name'),
+      emailEl = document.getElementById('email'),
+      phoneEl = document.getElementById('phone'),
+      messageEl = document.getElementById('message')
 
 contactForm.addEventListener('submit', saveFormData)
 
@@ -6,10 +10,10 @@ function saveFormData(e) {
     e.preventDefault();
     
     var formData = {
-      name: document.getElementById('name').value,
-      email: document.getElementById('email').value,
-      phone: document.getElementById('phone').value,
-      message: document.getElementById('message').value
+      name: nameEl.value,
+      email: emailEl.value,
+      phone: phoneEl.value,
+      message: messageEl.value
     };
   
     fetch('https://script.google.com/macros/s/AKfycbw587yzwv8f2GBtPgv_KbaaE_B4A0RcZL_jrsjY0OqexrBEqgokL5CnAlAgZmWNjnm7LQ/exec', {
@@ -21,9 +25,10 @@ function saveFormData(e) {
       body: JSON.stringify(formData),
     })
     .then(response => {
-      if (response.ok) {
-        console.log('Form data saved successfully');
-        // Optionally, you can show a success message or perform other actions here
+      if (response.status == "success") {
+        console.log(response.message);
+        alert(response.message + ' we will contact you shortly.')
+        contactForm.reset()
       } else {
         console.error('Failed to save form data:', response.statusText);
         // Optionally, you can show an error message or perform other actions here
