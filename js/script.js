6a887c82efa04e1e5533c9ced82991a20fc0604b
omsoffset <163588270+omsoffset@users.noreply.github.com>
2024-03-20 00:55:28 +0530

    Add files via upload
    favicon and contact from updated

---
+++ js/script.js
@@ -0,0 +1,36 @@
+const contactForm = document.getElementById('contact-form')
+
+contactForm.addEventListener('submit', saveFormData)
+
+function saveFormData(e) {
+    e.preventDefault();
+    
+    var formData = {
+      name: document.getElementById('name').value,
+      email: document.getElementById('email').value,
+      phone: document.getElementById('phone').value,
+      message: document.getElementById('message').value
+    };
+  
+    fetch('https://script.google.com/macros/s/AKfycbw587yzwv8f2GBtPgv_KbaaE_B4A0RcZL_jrsjY0OqexrBEqgokL5CnAlAgZmWNjnm7LQ/exec', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    })
+    .then(response => {
+      if (response.ok) {
+        console.log('Form data saved successfully');
+        // Optionally, you can show a success message or perform other actions here
+      } else {
+        console.error('Failed to save form data:', response.statusText);
+        // Optionally, you can show an error message or perform other actions here
+      }
+    })
+    .catch(error => {
+      console.error('Error saving form data:', error);
+      // Optionally, you can show an error message or perform other actions here
+    });
+  }
+  
